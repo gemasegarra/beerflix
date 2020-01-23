d24fe73f78775ee9b72f037817dd9733b38cb48a
gemasegarra <gema.segb@gmail.com>
2020-01-23 16:54:51 +0100

    render beer search results

---
+++ src/js/beers.js
@@ -0,0 +1,42 @@
+import api from './api.js'
+
+const { getBeers } = api();
+
+const beerTemplate = beer => {
+  return `
+  <div class="beercard">
+  <div class="beer-card">
+  <div class="image-container">
+    <img class="beer-pic" src="${beer.image}" alt='bottle-pic'/>
+  </div>
+  <div class="description">
+    <h1 class="title-section" id="beer-name">${beer.name}</h1>
+    <p class="description">${beer.description}</p>
+    <p class="year">First brewed: ${beer.firstBrewed}</p>
+  </div>
+</div>
+</div>
+</div>` 
+};
+
+// add heart button 
+// add link to detailed page
+
+const renderBeers = (element, items) => {
+  const htmlBeers = items.map(function (beer) {
+    return beerTemplate(beer);
+  }).join('');
+  element.innerHTML = htmlBeers;
+}
+
+const renderBeersDOM = async text => {
+  try {
+    const beers = await getBeers(text);
+    const beerSection = document.querySelector('#beerlist')
+    renderBeers(beerSection, beers)
+  } catch (err){
+    console.log(err)
+  }
+};
+
+export default renderBeersDOM;
