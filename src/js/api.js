b9d0867babb5709efe8d3588d1d174530daa146b
gemasegarra <gema.segb@gmail.com>
2020-01-23 16:54:25 +0100

    create api call

--- src/js/api.js
+++ src/js/api.js
@@ -1,31 +1,40 @@
-import APIKEY from './api-key.js';
+const APIKEY = '9H66017-H8VMX7C-NY13E10-8RAY4SW';
+const URL = 'https://beerflix-api.herokuapp.com/api/v1/'
 
 const apiCall = (API) => {
-  return {
-    getBeers: async text => {
-      try {
-        const endpoint = `https://beerflix-api.herokuapp.com/api/v1/beers?search=${text}&limit=10
-        `;
-        const response = await fetch(endpoint, {
-          method: 'GET',
-          headers: {
-          'X-API-KEY': APIKEY,
-          },
-        });
-        if (!response.ok) {
-          throw new Error('Error retrieving beers');
-        }
-        const data = await response.json();
-        const beers = data.map(result => {
-          return result.show;
-        });
-        return beers;
+  
+    return {
+      getBeers: async text => {
+        try {
+          const response = await fetch(`${URL}beers?search=${text}&limit=10`, {
+                    method: 'GET',
+                    headers: {
+                    'X-API-KEY': APIKEY,
+                   }})
+          if (!response.ok) {
+            throw new Error('Error retrieving beers')
+          }
+          const data = await response.json();
+          console.log(data.beers);
+          return data.beers
+
       } catch (err) {
         console.error(err.message);
         throw err;
-      }
-    },
-  };
-};
+      }},
+      getDetails: id => {
+      return fetch(`${URL}/${id}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`Error retrieving beer ${id}`);
+          }
+          return response.json();
+        })
+        .catch(err => {
+          console.error(err.message);
+          throw err;
+        });
+    }}
+}; 
+ export default apiCall;   
 
-export default apiCall;
