0b2c0d491441f2b76872384e4beb2f89c6c94cfa
gemasegarra <gema.segb@gmail.com>
2020-01-25 15:55:21 +0100

    add comments section

--- src/js/detailed-card.js
+++ src/js/detailed-card.js
@@ -2,34 +2,62 @@ import api from './api.js';
 
 const detailedTemplate = ({beerId, name, image, description, firstBrewed, brewersTips, contributedBy, likes, price}) => {
   return `
-  <div class="beercard">
-  <div class="beer-card" id="${beerId}">
-  <div class="image-container">
+  <div class="detail-section">
+  <div class="card" id="${beerId}">
+  <div class="img-container">
     <img class="beer-pic" src="${image}" alt='bottle-pic'/>
   </div>
   <div class="description">
     <h1 class="title-section" id="beer-name">${name}</h1>
     <p class="description">${description}</p>
     <p class="year">First brewed: ${firstBrewed}</p>
-    <p class="brewersTips">Brewer tip: ${brewersTips}</p>
+    <p class="brewersTips">Brewer tip: '${brewersTips}'</p>
     <p class="contributedBy">Contributed by ${contributedBy}</p>
     <p class="likes">${likes} ❤️</p>
     <p class="price">Price ${price} 💵</p>
-  </div>
 </div>
 </div> 
 </div>
 `
 };
 
-const { getDetails } = api()
+const commentTemplate = ({comment, date}) => {`
+  <div class="container">
+    <p>${comment}</p>
+    <p>${date}</p>
+  </div>
+  `
+}
+
+const commentsFormTemplate = `
+<div id="detail" class="comment-content">
+<div class="container">
+<h2>Comments</h2>
+<div id="commentslist">
+</div>
+</div>
+<form id="comment-form" method="POST" class="comment-form" novalidate>
+<div class="comment-input">
+  <input required id="comment" placeholder="Write your comment here!" class="input" type="text">
+</div>
+<button type="submit" class="button-s">Add comment</button>
+</form>
+</div>
+</div>`
+
+const { getDetails } = api();
 
 const renderDetail = async id => {
   try { 
     console.log('prueba')
     const detail = await getDetails(id);
+  
     const template = detailedTemplate(detail);
     const mainSection = document.querySelector('main');
+    const comments = document.querySelector('#details');
+    const beerList = document.querySelector('#beerlist');
+    beerList.innerHTML = "";
+    comments.innerHTML = commentsFormTemplate;
     mainSection.innerHTML = template;
   }
   catch (err) {
