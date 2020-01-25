8d1c27707df4feaa8875d3d11665f5030bd86718
gemasegarra <gema.segb@gmail.com>
2020-01-25 18:58:37 +0100

    localStorage changes

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,26 +1,26 @@
 import renderBeersDOM from './beers.js';
 import storage from './storage.js';
+import './ui.js';
 
 const { setItem, getItem } = storage('lStorage');
 export const STORAGE_TYPE = 'lStorage';
 
-export const SEARCH_INPUT_STORAGE_ID = 'search-input';
-export const FILTER_INPUT_STORAGE_ID = 'filter-input'
+export const INPUT_STORAGE_ID = 'input';
 
 const searchForm = document.querySelector('#search-box');
 const searchInput = document.querySelector('#search-beer');
 const filterForm = document.querySelector('#filter-box');
 const filterInput = document.querySelector('#filter');
 
-searchInput.value = getItem(SEARCH_INPUT_STORAGE_ID);
-filterInput.value = getItem(FILTER_INPUT_STORAGE_ID);
+searchInput.value = getItem(INPUT_STORAGE_ID);
+filterInput.value = getItem(INPUT_STORAGE_ID);
 
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
   if (searchInput.validity.valid) {
     console.log('Get beer named', searchInput.value)
     renderBeersDOM(searchInput.value);
-    setItem(SEARCH_INPUT_STORAGE_ID, searchInput.value)
+    setItem(INPUT_STORAGE_ID, searchInput.value)
   }
 });
 
@@ -30,6 +30,6 @@ filterForm.addEventListener('submit', e => {
   if (filterInput.validity.valid) {
     console.log(filterInput.value)
     renderBeersDOM(filterInput.value);
-    setItem(FILTER_INPUT_STORAGE_ID, filterInput.value)
+    setItem(INPUT_STORAGE_ID, filterInput.value)
   }
 });
